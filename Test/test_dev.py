c9484a83913f5a1fd44b211119c44948692f9531
nneehhaa123 <40603751+nneehhaa123@users.noreply.github.com>
2024-03-06 07:47:23 +0000

    added pred test

--- Test/test_dev.py
+++ Test/test_dev.py
@@ -1,8 +1,22 @@
 import pandas as pd
-
-
+import pickle
+import pytest 
 
 
 def test_attribute():
     df = pd.read_csv("./data/insurance_data.csv")
     assert df.shape[1] == 2
+
+@pytest.fixture()
+def model():
+    loaded_model = pickle.load(open("./Model/lr_model.pkl", "rb"))
+    return loaded_model
+
+def test_predict(model):
+
+    # load the model from disk
+    data = {"CLMAGE": 100}
+    df = pd.DataFrame(data, index=[0])
+    prediction = model.predict(df)
+    print(prediction)
+    assert prediction ==1
